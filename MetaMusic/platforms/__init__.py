8e6769a29ab9d1b8af4293b87cdf6066d50f9446
Probotsvip <219003520+Probotsvip@users.noreply.github.com>
2025-07-09 16:23:41 +0530

    Update __init__.py

--- MetaMusic/platforms/__init__.py
+++ MetaMusic/platforms/__init__.py
@@ -1,18 +1,15 @@
-
 from .Apple import AppleAPI
-from .Carbon import CarbonAPI  
 from .MusicAPI import MusicAPI
 from .Resso import RessoAPI
 from .Soundcloud import SoundAPI
 from .Spotify import SpotifyAPI
-from .Telegram import TeleAPI
+from .Telegram import TelegramAPI
 from .Youtube import YouTubeAPI
 
 Apple = AppleAPI
-Carbon = CarbonAPI
 MusicAPI = MusicAPI
 Resso = RessoAPI
 SoundCloud = SoundAPI
 Spotify = SpotifyAPI
-Telegram = TeleAPI
+Telegram = TelegramAPI
 YouTube = YouTubeAPI
